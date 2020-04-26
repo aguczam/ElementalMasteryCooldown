b31c174322ed009e8431e07752ad65cc04fcbd3d
Maczuga <xian@poczta.fm>
2020-04-26 16:05:23 +0200

    Update newRotationBase.lua

--- newRotationBase.lua
+++ newRotationBase.lua
@@ -1,10 +1,14 @@
 if magicSquare==nil then magicSquare=CreateFrame("Frame","MovableTextureFrame",UIParent)
+   magicSquare:SetMovable(true)
+   magicSquare:EnableMouse(true)
+   magicSquare:SetUserPlaced(true)
+   magicSquare:RegisterForDrag("LeftButton")
+   magicSquare:SetScript("OnDragStart", frame.StartMoving)
+   magicSquare:SetScript("OnDragStop", frame.StopMovingOrSizing)
    magicSquare:SetSize(64,64)
    magicSquare:SetPoint("TOPLEFT")
-   
    magicSquare.texture = magicSquare:CreateTexture(nil,"ARTWORK")
    magicSquare.texture:SetAllPoints(true)
-   
    magicSquare.texture:SetTexture(128/255,0,0,1) 
 end
 
@@ -14,5 +18,16 @@ function getSpellTexture(spellName)
    return icon
 end
 
-spellName = "War Stomp"
-magicSquare.texture:SetTexture(getSpellTexture(spellName)) 
+
+if constantSpam==nil then 
+   constantSpam = CreateFrame("Frame")
+   constantSpam:SetScript("OnUpdate", function(self, event, ...)
+         Rotation()
+   end)
+end
+
+function Rotation()
+   spellName=""
+   if GetSpellCooldown("Starsurge")==0 then spellName="Starsurge" end
+   magicSquare.texture:SetTexture(getSpellTexture(spellName)) 
+end
